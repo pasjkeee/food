a4ef43f2b7696a9ad648274738c6e53fdd653a20
Pasha <pasjkeee@gmail.ru>
2020-11-23 22:47:32 +0300

    End of native

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,54 +1,58 @@
-const   myTime = document.querySelectorAll('.timer__block'),
-        endTime = new Date(),
-        promTime = document.querySelector('.promotion__descr');
-        endTime.setMonth(endTime.getMonth() + 1, 0);
-        endTime.setHours(23, 59, 59, 999);
+const myTime = document.querySelectorAll('.timer__block'),
+    endTime = new Date(),
+    promTime = document.querySelector('.promotion__descr');
+endTime.setMonth(endTime.getMonth() + 1, 0);
+endTime.setHours(23, 59, 59, 999);
 const myMenu = document.querySelector('.tabcontainer'),
-      tabPhoto = myMenu.querySelectorAll('.tabcontent'),
-      menuList = myMenu.querySelectorAll('.tabheader__item'),
-      myButton = document.querySelectorAll('[data-model]'),
+    tabPhoto = myMenu.querySelectorAll('.tabcontent'),
+    menuList = myMenu.querySelectorAll('.tabheader__item'),
+    myButton = document.querySelectorAll('[data-model]'),
 
-      modal = document.querySelector('.modal'),
-      modalClose = modal.querySelector('.modal__close');
-      
-      function disable(tab) {
-      tab.forEach( (exp)=> {
-                exp.classList.add('hide');
-                exp.classList.remove('show', 'fade');
-      });}
-      disable(tabPhoto);
-      tabPhoto[0].classList.add('show');
-      tabPhoto[0].classList.remove('hide');
+    modal = document.querySelector('.modal'),
+    modalClose = modal.querySelector('.modal__close');
 
-     function removeCl (cl) {
-        cl.forEach( (item)=> {
-            item.classList.remove('tabheader__item_active');
-        });
-     }
+function disable(tab) {
+    tab.forEach((exp) => {
+        exp.classList.add('hide');
+        exp.classList.remove('show', 'fade');
+    });
+}
+disable(tabPhoto);
+tabPhoto[0].classList.add('show');
+tabPhoto[0].classList.remove('hide');
 
-   menuList.forEach( (item, i)=> {
-       item.addEventListener('click', (e)=> {
-           e.preventDefault();
-           console.log(i); 
-           removeCl(menuList);
-           item.classList.toggle('tabheader__item_active');
-           disable(tabPhoto);
-            tabPhoto[i].classList.add('show', 'fade');
-            tabPhoto[i].classList.remove('hide');
-        });
-       
-   });
+function removeCl(cl) {
+    cl.forEach((item) => {
+        item.classList.remove('tabheader__item_active');
+    });
+}
+
+menuList.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(i);
+        removeCl(menuList);
+        item.classList.toggle('tabheader__item_active');
+        disable(tabPhoto);
+        tabPhoto[i].classList.add('show', 'fade');
+        tabPhoto[i].classList.remove('hide');
+    });
+
+});
 
 
-function setZero(obj){
-    if(obj<10)
-    {
-        obj='0'+ obj;
+function setZero(obj) {
+    if (obj < 10) {
+        obj = '0' + obj;
     }
     return String(obj);
 }
 
-let Days = '', Hours = '', Minutes = '', Seconds = '';
+let Days = '',
+    Hours = '',
+    Minutes = '',
+    Seconds = '';
+
 function gettingTime() {
     let startTime = new Date();
     Days = endTime.getDate() - startTime.getDate();
@@ -60,62 +64,100 @@ function gettingTime() {
     Seconds = endTime.getSeconds() - startTime.getSeconds();
     Seconds = setZero(Seconds);
     let arr = [Days, Hours, Minutes, Seconds];
-    myTime.forEach ( (e, i) => {
+    myTime.forEach((e, i) => {
         e.firstElementChild.innerHTML = arr[i];
     });
 }
 
-setInterval (gettingTime, 999);
+setInterval(gettingTime, 999);
 promTime.innerHTML = "";
-promTime.innerHTML +=`Мы ценим каждого клиента и предлагаем вам стать одним из них на очень выгодных условиях.`;
-promTime.innerHTML +=`Каждому, кто закажет доставку питание на неделю, будет предоставлена скидка в размере <span>20%!</span>`;
-promTime.innerHTML +=`<br><br> Акция закончится ${endTime.toLocaleString("ru")} `;
+promTime.innerHTML += `Мы ценим каждого клиента и предлагаем вам стать одним из них на очень выгодных условиях.`;
+promTime.innerHTML += `Каждому, кто закажет доставку питание на неделю, будет предоставлена скидка в размере <span>20%!</span>`;
+promTime.innerHTML += `<br><br> Акция закончится ${endTime.toLocaleString("ru")} `;
 
 
 function closeModal() {
     modal.classList.add('hide');
     modal.classList.remove('show');
-    document.body.style.overflow="";
-    }
-myButton.forEach((e) => {
-            e.addEventListener('click', () => {
-                if (e.dataset.model == "btnOpen") {
-                    modal.classList.add('show');
-                    modal.classList.remove('hide');
-                    document.body.style.overflow="hide";
-                }
-                if (e.dataset.model == "btnCall") {
-                    alert("В разработке");
-                }
-            });
-            modalClose.addEventListener('click', ()=> {
-               closeModal();
-            });
-            modal.addEventListener('click', (event)=> {
-                if (event.target === modal) {
-                    closeModal();
-                }
-            });
+    document.body.style.overflow = "";
+}
 
-            document.addEventListener('keydown', (eve) => {
-                if(eve.code === "Escape" && modal.classList.contains("show")) {
-                    closeModal();
-                }
-            });
+function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = "hide";
+}
+myButton.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (e.dataset.model == "btnOpen") {
+            openModal();
+        }
+        if (e.dataset.model == "btnCall") {
+            alert("В разработке");
+        }
+    });
+    modalClose.addEventListener('click', () => {
+        closeModal();
+    });
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (eve) => {
+        if (eve.code === "Escape" && modal.classList.contains("show")) {
+            closeModal();
+        }
+    });
 });
 
 const docH = document.documentElement.scrollHeight;
-function crollClose () {
+
+function crollClose() {
     let myH = document.documentElement.scrollTop;
     const winH = window.innerHeight;
-    console.log("H", winH);
-    console.log(docH-myH);
-    if( docH-myH < winH ) {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow="hide";
+    if (docH - myH < winH) {
+        openModal();
         window.removeEventListener('scroll', crollClose);
     }
 }
 
 const meEv = window.addEventListener('scroll', crollClose);
+
+const menuListss = document.querySelector('.menu__field');
+const menuLists = menuListss.querySelector('.container');
+const menuPrice = menuListss.querySelectorAll('.menu__item-price');
+console.log(menuLists);
+
+class menuT {
+    constructor( menuTitle, menuText, menuPhoto, menuPrice, ...classes) {
+        this.menuTitle = menuTitle;
+        this.menuText = menuText;
+        this.menuPhoto = menuPhoto;
+        this.menuPrice = menuPrice;
+        this.classes = classes;
+    }
+
+    isertTitle() {
+       const element = document.createElement('div');
+       if(this.classes.length === 0) {
+         element.classList.add("menu__item");
+       } else {
+           this.classes.forEach ( e => element.classList.add(e) );
+       }
+       element.insertAdjacentHTML('beforeend', `
+       <img src="${this.menuPhoto}" alt="vegy">
+       <h3 class="menu__item-subtitle">${this.menuTitle}</h3>
+       <div class="menu__item-descr">${this.menuText}</div>
+       <div class="menu__item-divider"></div>
+       <div class="menu__item-price">
+           <div class="menu__item-cost">Цена:</div>
+           <div class="menu__item-total"><span>${this.menuPrice}</span> грн/день</div>
+       </div>`);
+        menuLists.append(element);
+    }
+}
+
+new menuT("Даня привет", "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!", "img/tabs/elite.jpg", "228", "menu__item").isertTitle();
+  
