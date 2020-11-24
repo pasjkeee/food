1cac36a783e176d1150d89356a00b77c9c695426
Pasha <pasjkeee@gmail.ru>
2020-11-24 03:15:09 +0300

    Send format json

--- dist/js/script.js
+++ dist/js/script.js
@@ -175,6 +175,34 @@ forms.forEach(item => {
     postData(item);
 });
 
+// function postData(form) {
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+
+//         const statusMessage = document.createElement('div');
+//         statusMessage.classList.add('status');
+//         statusMessage.textContent = message.loading;
+//         form.appendChild(statusMessage);
+
+//         const request = new XMLHttpRequest();
+//         request.open('POST', 'server.php');
+
+//        // request.setRequestHeader('Content-type', 'multipart/form-data'); Не надо так
+//         const formData = new FormData(form);
+
+//         request.send(formData);
+
+//         request.addEventListener('load', () => {
+//             if (request.status === 200) {
+//                 console.log(request.response);
+//                 statusMessage.textContent = message.success;
+//                 form.reset();
+//                 setTimeout( (() => statusMessage.remove()), 2000);
+//             } else { statusMessage.textContent = message.failure; }
+//         });
+//     });
+// }
+
 function postData(form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -187,10 +215,16 @@ function postData(form) {
         const request = new XMLHttpRequest();
         request.open('POST', 'server.php');
 
-       // request.setRequestHeader('Content-type', 'multipart/form-data');
+         request.setRequestHeader('Content-type', 'application/json'); 
         const formData = new FormData(form);
 
-        request.send(formData);
+        const object = {};
+        formData.forEach(function(value,key){
+            object[key] = value;
+        });
+
+        const json = JSON.stringify(object);
+        request.send(json);
 
         request.addEventListener('load', () => {
             if (request.status === 200) {
